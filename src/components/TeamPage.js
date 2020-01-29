769d7fa7e7b12b6ce4d43a9013c8dbbdb156b5d4
Teddy <tedrandby@gmail.com>
2020-01-29 13:21:49 -0500

    help

--- src/components/TeamPage.js
+++ src/components/TeamPage.js
@@ -4,20 +4,29 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function TeamPage(props) {
   const renderTeamButton = team => {
     return (
-      <div className="box">
-        <p className="subtitle is-4">{props.game.home.name}</p>
-        <div className="buttons has-addons is-centered">
-          <button className="button is-success is-large">
-            <span>
-              <FontAwesomeIcon icon="plus" />
-            </span>
-          </button>
-          <button className="button is-danger is-large">
-            <span>
-              <FontAwesomeIcon icon="minus" />
-            </span>
-          </button>
+        
+      <div className="content has-text-centered">
+          <div className="content">
+        <p class="title is-1">{props.game.away.name}</p>
+          <p class="subtitle is-1">{props.game.away.score}</p>
         </div>
+        <div className="content">
+          <p class="title is-1">{props.game.home.name}</p>
+          <p class="subtitle is-1">{props.game.home.score}</p>
+          <div className="buttons has-addons is-centered">
+          <button className="button is-danger is-large">
+              <span>
+                <FontAwesomeIcon icon="minus" />
+              </span>
+            </button>
+            <button className="button is-success is-large">
+              <span>
+                <FontAwesomeIcon icon="plus" />
+              </span>
+            </button>
+          </div>
+        </div>
+        
       </div>
     );
   };
